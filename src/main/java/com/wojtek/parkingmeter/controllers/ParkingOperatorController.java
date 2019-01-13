e9d4105445a6164b4359d7218a055c68f4648fe4
WojciechWeg <wojtek.weg@gmail.com>
2019-01-13 18:29:21 +0100

    controller skeleton part 2

--- src/main/java/com/wojtek/parkingmeter/controllers/ParkingOperatorController.java
+++ src/main/java/com/wojtek/parkingmeter/controllers/ParkingOperatorController.java
@@ -1,4 +1,20 @@
 package com.wojtek.parkingmeter.controllers;
 
+import com.wojtek.parkingmeter.repositories.ParkingOperatorRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ParkingOperatorController {
+
+    private final ParkingOperatorRepository parkingOperatorRepository;
+
+    public ParkingOperatorController(ParkingOperatorRepository parkingOperatorRepository) {
+        this.parkingOperatorRepository = parkingOperatorRepository;
+    }
+
+    @GetMapping("{id}/check/{id_ticket}")
+    public void  checkIfStarted (@PathVariable Long id, @PathVariable Long id_ticket ){return;}
+
 }
--- src/main/java/com/wojtek/parkingmeter/controllers/TicketController.java
+++ src/main/java/com/wojtek/parkingmeter/controllers/TicketController.java
@@ -1,4 +1,29 @@
 package com.wojtek.parkingmeter.controllers;
 
+import com.wojtek.parkingmeter.repositories.TicketRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TicketController {
+
+
+    private final TicketRepository ticketRepository;
+
+    public TicketController(TicketRepository ticketRepository) {
+        this.ticketRepository = ticketRepository;
+    }
+
+    @GetMapping("/{id}/start")
+    public void startTicket(@PathVariable Long id){return;}
+
+    @GetMapping("/{id}/stop")
+    public void stopTicket(@PathVariable Long id){return;}
+
+    @GetMapping("/id/check_charge")
+    public void checkCharge(){return;}
+
+
+
 }
